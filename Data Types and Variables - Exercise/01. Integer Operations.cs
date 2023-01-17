34062eac15c0ce11fa0430e9222ca8d46efbf8e3
denev88 <119948803+denev88@users.noreply.github.com>
2023-01-17 21:28:47 +0200

    Data Types and Variables - Exercise
    This is Data Types and Variables - Exercise solution problem solving.

---
+++ Data Types and Variables - Exercise/01. Integer Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          // · First number will be in the range[-2, 147, 483, 648… 2, 147, 483, 647]
+          //
+          // · Second number will be in the range[-2, 147, 483, 648… 2, 147, 483, 647]
+          // 
+          // · Third number will be in the range[-2, 147, 483, 648… 2, 147, 483, 647]
+          // 
+          // · Fourth number will be in the range[-2, 147, 483, 648… 2, 147, 483, 647]
+
+          int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int result = (firstNumber + secondNumber) / thirdNumber * fourthNumber;
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/02. Sum Digits.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int nNum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            while (nNum != 0)
+            {
+               sum += nNum % 10;
+                nNum /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
