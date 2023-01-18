66610489f3efdc2613d16bc5c26438d6cb131985
denev88 <119948803+denev88@users.noreply.github.com>
2023-01-18 16:36:58 +0200

    Data Types and Variables - Exercise

---
+++ Data Types and Variables - Exercise/05. Print Part Of ASCII Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05._Print_Part_Of_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startInput = int.Parse(Console.ReadLine());
+            int endInput = int.Parse(Console.ReadLine());
+
+            for (int i = startInput; i <= endInput; i++)
+            {
+                char symbol = (char)i;
+                Console.Write($"{symbol} ");
+            }
+        }
+    }
+}
---
+++ Data Types and Variables - Exercise/06. Triples of Latin Letters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06._Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = int.Parse(Console.ReadLine());
+
+            // for (char i = 'a'; i <'a'+ n; i++)
+            // {
+            //     for (char j = 'a'; j < 'a'+ n; j++)
+            //     {
+            //         for (char k = 'a'; k < 'a'+ n; k++)
+            //         {
+            //
+            //             Console.WriteLine($"{i}{j}{k}");
+            //            
+            //         }
+            //
+            //     }
+
+            for (int i = 1; i < 1 + n; i++)
+            {
+                for (int j = 2; j < 2 + n; j++)
+                {
+                    for (int k = 3; k < 3 + n; k++)
+                    {
+
+                        Console.WriteLine($"{i}{j}{k}");
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
