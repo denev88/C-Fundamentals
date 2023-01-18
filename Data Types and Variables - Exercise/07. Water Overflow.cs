af7e8febbac912cb9ba54fa9f57bf6101c4018d7
denev88 <119948803+denev88@users.noreply.github.com>
2023-01-18 16:40:07 +0200

    Update 07. Water Overflow.cs

--- Data Types and Variables - Exercise/07. Water Overflow.cs
+++ Data Types and Variables - Exercise/07. Water Overflow.cs
@@ -6,7 +6,26 @@ namespace _07._Water_Overflow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           const int  capacity = 255;
+            int nLine = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < nLine; i++)
+            {
+                int include = int.Parse(Console.ReadLine());
+
+                if(include + sum > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    sum += include;
+                }
+              
+            }
+            Console.WriteLine($"{sum}");
+
         }
     }
 }
