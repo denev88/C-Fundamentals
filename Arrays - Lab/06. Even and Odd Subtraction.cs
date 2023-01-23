87c2e16fa27af6e55371abf487d6e6180e08c05d
denev88 <119948803+denev88@users.noreply.github.com>
2023-01-23 15:26:16 +0200

    Arrays - Lab
    Arrays - Lab exercises problem solving

---
+++ Arrays - Lab/06. Even and Odd Subtraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int oddSum = 0;
+            int evenSum = 0;
+
+          foreach (int i in number)
+            {
+                if(i % 2 == 0) 
+                {
+                    oddSum += i;
+                }
+                else
+                {
+                    evenSum += i;
+                }
+            }
+          int totalSum = oddSum - evenSum;
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ Arrays - Lab/07. Equal Arrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr1 = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            double[] arr2 = Console.ReadLine().Split().Select(double.Parse).ToArray();
+                double sum = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+
+                    sum += arr1[i];
+
+                    if (i == arr1.Length - 1)
+                    {
+                        Console.WriteLine($"Arrays are identical. Sum: {sum}");
+                    }
+                }
+            }
+
+        }
+    }
+}
