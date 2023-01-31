ccf065abf2421e71b5e92ce8eeef87f25534c55d
denev88 <119948803+denev88@users.noreply.github.com>
2023-01-31 16:34:59 +0200

    Arrays - Exercise

---
+++ Arrays - Exercise/06. Equal Sum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        private static object array;
+
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += arr[j];
+                }
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    rightSum += arr[k];
+                }
+                if(leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+                
+               
+            }
+            Console.WriteLine("no");
+
+        }
+            
+        
+    }
+}
---
+++ Arrays - Exercise/07. Max Sequence of Equal Elements.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        private static int i;
+
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int maxElement = 0;
+            int currentElement = 1;
+            int element = 0;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+
+
+
+                if (input[i] == input[i + 1])
+                {
+                    currentElement++;
+                }
+                else
+                {
+                    currentElement = 1;
+                }
+
+                if (currentElement > maxElement)
+                {
+                    maxElement = currentElement;
+
+                    element = input[i];
+                }
+  
+              
+            }
+            for (int j = 0; j < maxElement; j++)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
+
