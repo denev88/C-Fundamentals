037b842838a2d2053493388762f5323902cdb556
denev88 <119948803+denev88@users.noreply.github.com>
2023-02-13 21:39:51 +0200

    Methods - Lab

---
+++ Methods - Lab/02. Grades.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	2.00 – 2.99 - "Fail"
+            //•	3.00 – 3.49 - "Poor"
+            //•	3.50 – 4.49 - "Good"
+            //•	4.50 – 5.49 - "Very good"
+            //•	5.50 – 6.00 - "Excellent"
+
+            double grade = double.Parse(Console.ReadLine());
+
+            Program.grade(grade);
+
+        }
+
+        private static void grade(double grade)
+        {
+            if (grade >= 2.00 && grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
---
+++ Methods - Lab/03. Calculations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = Console.ReadLine();
+
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            if (action == "add")
+            {
+                Add(firstNum, secondNum);
+            }
+            if (action == "multiply")
+            {
+                Multiply(firstNum, secondNum);
+            }
+            if (action == "subtract")
+            {
+                Subtract(firstNum, secondNum);
+            }
+            if (action == "divide")
+            {
+                Divide(firstNum, secondNum);
+            }
+
+        }
+
+        private static void Divide(int firstNum, int secondNum)
+        {
+            int result = firstNum / secondNum;
+            Console.WriteLine(result);
+        }
+
+        private static void Subtract(int firstNum, int secondNum)
+        {
+            int result = firstNum - secondNum;
+            Console.WriteLine(result);
+        }
+
+        private static void Multiply(int firstNum, int secondNum)
+        {
+            int result = firstNum * secondNum;
+            Console.WriteLine(result);
+        }
+
+        private static void Add(int firstNum, int secondNum)
+        {
+            int result = firstNum + secondNum;
+            Console.WriteLine(result);
+        }
+
+
+    }
+
+}
+
