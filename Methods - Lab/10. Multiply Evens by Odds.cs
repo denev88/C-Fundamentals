ab138474f433725d6e602d62419300e0a166f333
denev88 <119948803+denev88@users.noreply.github.com>
2023-02-21 13:48:17 +0200

    Methods - Lab

---
+++ Methods - Lab/10. Multiply Evens by Odds.cs
@@ -0,0 +1,52 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int num = Math.Abs(int.Parse(Console.ReadLine()));
+        int result = FindResult(num);
+        Console.WriteLine(result);
+    }
+
+    static int FindResult(int num)
+    {
+        int rezult = FindSumOfOdds(num) * FindSumOfEvens(num);
+        return rezult;
+    }
+
+    static int FindSumOfOdds(int num)
+    {
+        int sumOfOdds = 0;
+
+        while (num > 0)
+        {
+
+            int nextNum = num % 10;
+            num /= 10;
+            if (nextNum % 2 != 0)
+            {
+                sumOfOdds += nextNum;
+            }
+
+        }
+
+        return sumOfOdds;
+    }
+
+    static int FindSumOfEvens(int num)
+    {
+        int sumOfEvens = 0;
+        while (num > 0)
+        {
+            int nextNum = num % 10;
+            num /= 10;
+            if (nextNum % 2 == 0)
+            {
+                sumOfEvens += nextNum;
+            }
+
+        }
+
+        return sumOfEvens;
+    }
+}
