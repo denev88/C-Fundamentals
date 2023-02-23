db0d119686ddcdc5e236ff2bdd75f387ae69916b
denev88 <119948803+denev88@users.noreply.github.com>
2023-02-23 20:40:06 +0200

    Methods - Exercise

---
+++ Methods - Exercise/02. Vowels Count.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word == "a,o,u,e,i")
+                sum += word[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ Methods - Exercise/05. Add and Subtract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int result = ReturnSum(number, number2);
+            Subtract(result, number3);
+           
+        }
+
+        private static void Subtract(int result, int number3) => 
+            Console.WriteLine(result - number3);
+        
+
+        private static int ReturnSum(int number, int number2) => number + number2;
+       
+       
+    }
+}
